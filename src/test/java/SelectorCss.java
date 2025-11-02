246543e9076ef277476cab54d8683af7390c1fd9
ILIA <brakerilya@yandex.ru>
2025-11-02 14:06:10 +0300

    Find elements by cssSelector and fill out the login form

--- src/test/java/SelectorCss.java
+++ src/test/java/SelectorCss.java
@@ -27,6 +27,23 @@ public class SelectorCss {
         inputPassword.sendKeys("Qwerty123!");
         btnLogin1.click();
     }
+    @Test
+    public void ilCarro(){
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://ilcarro.web.app/search ");
+        WebElement btnSearch=driver.findElement(By.cssSelector("a[href='/search']"));
+        WebElement btnLetTheCarWork=driver.findElement(By.cssSelector("a[href='/let-car-work']"));
+        WebElement btnTermsOfUse=driver.findElement(By.cssSelector("a[href=\"/terms-of-use\"]"));
+        WebElement btnSignUp=driver.findElement(By.cssSelector("a[ng-reflect-router-link='registration']"));
+        WebElement btnLogin=driver.findElement(By.cssSelector("a[ng-reflect-router-link='login']"));
+        btnLogin.click();
+        WebElement inputEmail=driver.findElement(By.id("email"));
+        WebElement inputPassword=driver.findElement(By.id("password"));
+        inputEmail.sendKeys("bilbo_baggins_12345@mail.com");
+        inputPassword.sendKeys("Password123!");
+    }
     static void pause(int time){
         try {
             Thread.sleep(time*1000L);
